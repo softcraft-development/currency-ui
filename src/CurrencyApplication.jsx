f6f042c90fd43fd09cd18a44059ad78cff6b3f83
Craig Walker <github@softcraft.ca>
2017-08-20 18:59:17 +0000

    Some comments about principles

--- src/CurrencyApplication.jsx
+++ src/CurrencyApplication.jsx
@@ -23,6 +23,15 @@ export class CurrencyApplication extends React.Component {
     }
 
     render() {
+        // This is obviously just the start of the app, and there'd be a fair amount of work left.
+        // This doesn't even give much of a sense of my overall design techniques; I can discuss
+        // those in detail during the interview.
+        // However, it's starting to illustrate a few key principles of a React-based web application:
+        //  - Manage your application state in parent components, upstream of the components that do most of the rendering
+        //      - I prefer using Redux for state management on large apps, not in-component state management like I'm doing here.
+        //  - Split controls into their individual components where appropriate. 
+        //      - Keep their behaviour encapsulated
+        //      - Pay attention to prop & state updates to avoid unnecessary rerendering
         return <div id='currency-ui'>
             <h1>Currency UI</h1>
             <VisualizationSelector type={this.state.visualization} onChange={this.setVisualization} />
